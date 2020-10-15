be55ac74a44103fbc43b60da401af94798697ed2
luxiang <xiaoxiaomiantianwo@163.com>
2020-10-15 10:38:04 +0800

    播放时测试

---
+++ VedioPlayer/src/videoplayer/videoplayer.h
@@ -0,0 +1,32 @@
+﻿#ifndef VIDEOPLAYER_H
+#define VIDEOPLAYER_H
+
+#include <QThread>
+#include <QImage>
+
+class VideoPlayer : public QThread
+{
+    Q_OBJECT
+
+public:
+    explicit VideoPlayer();
+    ~VideoPlayer();
+
+    void setFileName(QString path)
+	{
+		mFileName = path;
+	}
+
+    void startPlay();
+
+signals:
+    void sig_GetOneFrame(QImage); //没获取到一帧图像 就发送此信号
+
+protected:
+    void run();
+
+private:
+    QString mFileName;
+};
+
+#endif // VIDEOPLAYER_H
